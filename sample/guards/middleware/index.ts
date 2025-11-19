3dd00974cdddab092dddcf89c069199fa9b21b43
Sunny <77013204+sunny-bhakta@users.noreply.github.com>
2025-11-19 15:59:06 +0530

    Delete sample/guards/middleware directory

--- sample/guards/middleware/index.ts
+++
@@ -1,5 +0,0 @@
-export { LoggingMiddleware } from './logging.middleware';
-export { RequestContextMiddleware } from './request-context.middleware';
-export { RateLimitMiddleware } from './rate-limit.middleware';
-export { SecurityHeadersMiddleware } from './security-headers.middleware';
-export { MiddlewareModule } from './middleware.module';
