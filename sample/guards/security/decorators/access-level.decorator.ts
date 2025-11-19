9576f71f23d67c57b8f9230edb323769aaeaa7dc
Sunny <77013204+sunny-bhakta@users.noreply.github.com>
2025-11-19 15:56:09 +0530

    Delete sample/guards/security directory

--- sample/guards/security/decorators/access-level.decorator.ts
+++
@@ -1,7 +0,0 @@
-import { SetMetadata } from "@nestjs/common";
-import { AccessLevel } from "../../enums/access-level.enum";
-
-export const ACCESS_LEVEL_KEY = 'accessLevel';
-export const RequireAccessLevel = (level: AccessLevel) => {
-    return SetMetadata(ACCESS_LEVEL_KEY, level);
-}
--- sample/guards/security/decorators/skip-auth.decorator.ts
+++
@@ -1,7 +0,0 @@
-import { SetMetadata } from "@nestjs/common";
-
-export const SKIP_AUTH_KEY = 'skipAuth';
-
-export const SkipAuth = () => {
-    return SetMetadata(SKIP_AUTH_KEY, true);
-}
